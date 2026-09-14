beb8774c5db3b6a3e1ad81aeec52e2e25867fab5
Muriuki00 <117885940+Muriuki00@users.noreply.github.com>
2026-09-14 16:08:50 +0300

    Connect Firebase configuration

--- js/firebase.js
+++ js/firebase.js
@@ -12,7 +12,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  // Firebase configuration will go here
+  apiKey: "AIzaSyCKTh3mipwCyjwnUeffmE_cpHbYwmWb8dI",
+  authDomain: "sales-tracker-24b20.firebaseapp.com",
+  projectId: "sales-tracker-24b20",
+  storageBucket: "sales-tracker-24b20.firebasestorage.app",
+  messagingSenderId: "697672956513",
+  appId: "1:697672956513:web:a06c5020b93eb7bba58820"
 };
 
 const app = initializeApp(firebaseConfig);
